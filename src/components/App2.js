d2ec1f2fc315ae042ccb399f359168423255f86d
Gabrielius <gdobrovolskis97@gmail.com>
2021-05-20 17:17:58 +0100

    corrected server fetch

--- src/components/App2.js
+++ src/components/App2.js
@@ -22,21 +22,21 @@ const App2 = () => {
     getTasks();
   }, []);
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch("http://18.135.107.127:5000/tasks");
     const data = await res.json();
 
     return data;
   };
 
     const fetchTask = async (id) => {
-      const res = await fetch(`http://localhost:5000/tasks/${id}`);
+      const res = await fetch(`http://18.135.107.127:5000/tasks/${id}`);
       const data = await res.json();
 
       return data;
     };
 
   const deleteTask = async(id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {method: "DELETE"});
+    await fetch(`http://18.135.107.127:5000/tasks/${id}`, { method: "DELETE" });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
@@ -44,7 +44,7 @@ const App2 = () => {
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder};
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {method: "PUT", headers: {"Content-type": "application/json"}, body: JSON.stringify(updTask)});
+    const res = await fetch(`http://18.135.107.127:5000/tasks/${id}`, {method: "PUT", headers: {"Content-type": "application/json"}, body: JSON.stringify(updTask)});
 
     const data = await res.json();
     setTasks(
@@ -54,10 +54,12 @@ const App2 = () => {
     );
   };
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {method: "POST", headers: {
-      "Content-type": "application/json"},
-      body: JSON.stringify(task)
-    
+    const res = await fetch("http://18.135.107.127:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
     });
 
     const data = await res.json();
